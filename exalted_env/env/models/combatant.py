a397bf4ecf64007896abe693fc1ae1e6d6339cf5
bartkusa <github@bartkusa.com>
2026-03-30 10:44:40 -0400

    Add Combatant.wound_penalty, dv* properties

--- exalted_env/env/models/combatant.py
+++ exalted_env/env/models/combatant.py
@@ -1,4 +1,5 @@
 from enum import Enum
+import math
 
 from exalted_env.env.models.armor import Armor, noArmor
 from exalted_env.env.models.character import Character
@@ -34,3 +35,44 @@ class Combatant:
         self.armor = armor or noArmor
         self.weapon1 = weapon
         self.weapon2: Weapon | None = fists
+
+    @property
+    def dv_parry(self) -> int:
+        base_pool = self.character.dexterity + self.character.melee
+        return max(
+            0,
+            (
+                math.ceil(base_pool / 2)
+                + self.weapon1.defense
+                + self.wound_penalty
+                + self.defense_modifier
+            ),
+        )
+
+    @property
+    def dv_evasion(self) -> int:
+        base_pool = self.character.dexterity + self.character.dodge
+        return max(
+            0,
+            (
+                math.ceil(base_pool / 2)
+                + self.weapon1.defense
+                + self.armor.mobility
+                + self.wound_penalty
+                + self.defense_modifier
+            ),
+        )
+
+    @property
+    def dv(self) -> int:
+        return max(self.dv_evasion, self.dv_parry)
+
+    @property
+    def wound_penalty(self) -> int:
+        assert self.damage >= 0
+
+        count_health_levels = len(self.character.health_levels)
+        assert len(count_health_levels) > 0
+
+        index = self.damage if (self.damage < count_health_levels) else -1
+        return self.character.health_levels[index]
