fa845e45b0fe08ee056939c08c6abf403280a601
bartkusa <github@bartkusa.com>
2026-03-30 10:43:36 -0400

    [fix] armor mobility penalties should be negative

--- exalted_env/env/models/armor.py
+++ exalted_env/env/models/armor.py
@@ -10,5 +10,5 @@ class Armor:
 
 noArmor = Armor(soak=0, mobility_penalty=0, name="No Armor")
 ltArmor = Armor(soak=3, mobility_penalty=0, name="Light Armor")
-mdArmor = Armor(soak=5, mobility_penalty=1, name="Medium Armor")
-hvArmor = Armor(soak=7, mobility_penalty=1, name="Heavy Armor")
+mdArmor = Armor(soak=5, mobility_penalty=-1, name="Medium Armor")
+hvArmor = Armor(soak=7, mobility_penalty=-2, name="Heavy Armor")
