1238d0424463e7b6c91f8fec9502f338d87a6a23
bartkusa <github@bartkusa.com>
2026-03-31 01:42:09 -0400

    If initiative is tied, randomly pick next combatant

--- exalted_env/env/rules/who_is_next.py
+++ exalted_env/env/rules/who_is_next.py
@@ -1,6 +1,7 @@
+from random import shuffle
+
 from exalted_env.env.models.combatant import Combatant
 from exalted_env.env.models.game_1on1_combat import Game1On1Combat
-
 from exalted_env.env.rules.round_start import round_start
 
 
@@ -10,17 +11,31 @@ def who_is_next(game: Game1On1Combat) -> Combatant | None:
 
     Pseudocode:
     - find the combatant who has not gone this round, with the highest initiative
+      - if there's a tie, randomly pick someone tied for most init
     - if everyone has gone, run round_start(game) and retry
     """
 
     def pick_highest_init_not_gone() -> Combatant | None:
-        next_actor: Combatant | None = None
+        next_actors: list[Combatant] | None = None
+
         for combatant in game.combatants:
             if combatant.took_turn:
                 continue
-            if next_actor is None or combatant.initiative > next_actor.initiative:
-                next_actor = combatant
-        return next_actor
+            elif next_actors is None:
+                next_actors = [combatant]
+            elif combatant.initiative < next_actors[0].initiative:
+                continue
+            elif combatant.initiative == next_actors[0].initiative:
+                next_actors.append(combatant)
+            else:
+                next_actors = [combatant]
+
+        if next_actors:
+            if len(next_actors) > 1:
+                shuffle(next_actors)
+            return next_actors[0]
+        else:
+            return None
 
     next_actor = pick_highest_init_not_gone()
     if next_actor is not None:
